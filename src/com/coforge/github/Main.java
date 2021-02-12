dff800bca8f34f94c01437f04a971e23f052bdec
Amber <agrawalamber01@gmail.com>
2021-02-12 10:56:32 +0530

    test case print

--- src/com/coforge/github/Main.java
+++ src/com/coforge/github/Main.java
@@ -3,6 +3,6 @@ package com.coforge.github;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Help me I want to quit java");
     }
 }
